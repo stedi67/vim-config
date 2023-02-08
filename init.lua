a073f79b3b47d114dfab5f0d71cd8bde93917709
std <stephan.diehl@ableton.com>
2023-02-08 18:32:48 +0100

    working linux neovim config with language server

--- init.lua
+++ init.lua
@@ -15,6 +15,11 @@ require('packer').startup(function(use)
   -- Gruvbox theme
   use 'ellisonleao/gruvbox.nvim'
 
+  -- Mason (installs language servers for example)
+  use { "williamboman/mason.nvim",
+  	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",}
+
 -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then
@@ -39,8 +44,11 @@ if is_bootstrap then
   return
 end
 
+vim.g.mapleader = ','
 -- Set colorscheme
 vim.o.termguicolors = true
+
+-- gruvbox setup
 require('gruvbox').setup({
   undercurl = true,
   underline = true,
@@ -64,3 +72,53 @@ require('gruvbox').setup({
   transparent_mode = false,
 })
 vim.cmd([[colorscheme gruvbox]])
+
+-- Global settings
+
+vim.g.smartindent = true
+vim.g.tabstop = 4
+vim.g.shiftwidth = 4
+vim.g.expandtab = true
+vim.g.laststatus = 2
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', '<Leader>lk', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<Leader>lg', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', '<Leader>lb', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+
+-- Mason setup
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("lspconfig").pyright.setup{
+    on_attach = on_attach,
+}
