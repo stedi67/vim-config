7999c405a082dc8799b0fe49be0643d2b77ad968
Stephan Diehl <stephan.diehl@ableton.com>
2023-02-22 09:35:37 +0100

    Added gruvbox palettes

--- init.lua
+++ init.lua
@@ -179,3 +179,12 @@ require('nvim-treesitter.configs').setup({
     'rust',
   },
 })
+
+-- display diagnostic messages as floating text
+vim.diagnostic.config({
+  float = { source = "always", border = border },
+  virtual_text = false,
+  signs = true,
+})
+
+vim.cmd('autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})')
